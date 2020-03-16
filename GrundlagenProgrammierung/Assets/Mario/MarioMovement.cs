35e70a461a17860a9254aabb91ca23a4af90d6e6
RameshBettge <bettge.r@gmx.de>
2020-03-16 13:58:49 +0100

    ADDED: Mario Movement (Day6 Content)

---
+++ GrundlagenProgrammierung/Assets/Mario/MarioMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarioMovement : MonoBehaviour
+{
+    public float moveDistancePerSecond = 8;
+    public float maxFallSpeed = 9;
+
+    public float initialJumpVelocity = 20;
+
+    public float low_JumpVelocityDecrease = 20;
+    public float high_JumpVelocityDecrease = 80;
+
+    float verticalVelocity;
+
+    float horizontalInput;
+
+    Vector3 targetPosition;
+
+
+    void FixedUpdate()
+    {
+        targetPosition = transform.position;
+
+        SetHorizontalMovement();
+        SetVerticalMovement();
+
+        GetComponent<Rigidbody>().MovePosition(targetPosition);
+    }
+
+    void SetHorizontalMovement()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 horizontalMoveVector = new Vector3(horizontalInput, 0, 0);
+
+        MoveInDirection(horizontalMoveVector, moveDistancePerSecond);
+    }
+
+    void SetVerticalMovement()
+    {
+        bool spaceIsPressedDown = Input.GetKeyDown(KeyCode.Space);
+        if (spaceIsPressedDown)
+        {
+            Jump();
+        }
+
+        MoveInDirection(new Vector3(0, 1, 0), verticalVelocity);
+
+        float jumpVelocityDecrease = high_JumpVelocityDecrease;
+        bool spaceIsPressed = Input.GetKey(KeyCode.Space);
+        if (spaceIsPressed)
+        {
+            if (verticalVelocity > 0)
+            {
+                jumpVelocityDecrease = low_JumpVelocityDecrease;
+            }
+        }
+
+        verticalVelocity -= jumpVelocityDecrease * Time.fixedDeltaTime;
+
+        if (verticalVelocity < -maxFallSpeed)
+        {
+            verticalVelocity = -maxFallSpeed;
+        }
+    }
+
+    void Jump()
+    {
+        verticalVelocity = initialJumpVelocity;
+    }
+
+    void MoveInDirection(Vector3 moveVector, float speedMultiplier)
+    {
+        Vector3 deltaVector = moveVector * Time.fixedDeltaTime * speedMultiplier;
+        targetPosition += deltaVector;
+    }
+}
---
+++ GrundlagenProgrammierung/Assets/Woche1/Wiederholung.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiederholung : MonoBehaviour
+{
+    // Typ Name = Wert;
+    // Typ Name;
+
+    public int publicInt = 1;
+
+    int myNumber = 2;
+    int defaultNumber;
+
+    float myFloat = 10.12f;
+
+    string myName = "Steven";
+
+    public Transform anderesTransform;
+
+    public GameObject unserCube;
+
+    MeshRenderer andererRenderer;
+
+
+
+    Color myColor = new Color(1, 1, 1);
+
+    float speed = 0.1f;
+
+   
+
+    void Start()
+    {
+        andererRenderer = unserCube.GetComponent<MeshRenderer>();
+        andererRenderer.material.color = new Color(0, 0, 1);
+
+
+        Transform anderesTransform = unserCube.transform;
+        anderesTransform.position = new Vector3(0, 0, 0);
+
+        BoxCollider andererCollider = unserCube.GetComponent<BoxCollider>();
+        AssignRandomColor randomColorAssigner = unserCube.GetComponent<AssignRandomColor>();
+        PositionColor myPositionColor = unserCube.GetComponent<PositionColor>();
+
+
+        PublicColor meineColor = unserCube.GetComponent<PublicColor>();
+        meineColor.myColor = new Color(1, 0, 0);
+
+
+        int newDoubledInt = DoubleGivenInt(2);
+
+        print(newDoubledInt);
+
+
+    }
+
+
+
+    int DoubleGivenInt(int givenInt)
+    {
+        int doubledInt = givenInt * 2;
+
+        return doubledInt;
+    }
+
+    private void Update()
+    {
+        publicInt += 1;
+
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 moveVector = new Vector3(1, 0, 0);
+        transform.position = transform.position + moveVector * speed;
+    }
+
+
+    int AddNumbers(int a, int b)
+    {
+        int sum = a + b;
+
+        return sum;
+    }
+
+    int MultiplyNumbers(int a, int b, int c)
+    {
+        int ergebnis = a * b * c;
+
+        return ergebnis;
+    }
+
+    // Rückgabetyp EinName Parameter(typ ParameterName)
+    void MoveTransform(Transform givenTransform)
+    {
+        givenTransform.position += new Vector3(1, 0, 0);
+    }
+}
